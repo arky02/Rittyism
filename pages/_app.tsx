4a4a7425d64699ef09c079ca2a3514a43bd17b4e
arky02 <kyean07@naver.com>
2025-01-18 20:00:29 +0900

    feat: rittyism 기본 배경 이미지 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Image from "next/image";
+import Background from "/public/images/background.svg";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className="flex justify-center items-center h-screen w-screen flex-col gap-[65px]">
+      <Image
+        src={Background}
+        className="w-full h-full object-cover absolute -z-10"
+        alt="background"
+      />
+      <Component {...pageProps} />
+    </main>
+  );
 }
