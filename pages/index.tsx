f33513372a70e6a668a2529a998f07421f64eef2
arky02 <kyean07@naver.com>
2025-01-18 20:03:05 +0900

    feat(IndexPage): 메인페이지

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,114 +1,69 @@
+import React, { useState } from "react";
+
+import Button from "@/components/Button";
+import RittyLogo from "@/components/RittyLogo";
+import { useRouter } from "next/router";
+import YellowRitty from "/public/images/ritty/yellow.svg";
+import GrayRitty from "/public/images/ritty/gray.svg";
+import WhiteRitty from "/public/images/ritty/white.svg";
+import BlackRitty from "/public/images/ritty/black.svg";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const IndexPage = () => {
+  const router = useRouter();
+  const [selectedRitty, setSelectedRitty] = useState<
+    "gray" | "yellow" | "white" | "black" | null
+  >(null);
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  const RittyTypeSelect = ({
+    rittyType,
+    rittyImg,
+  }: {
+    rittyType: "gray" | "yellow" | "white" | "black";
+    rittyImg: string;
+  }) => {
+    return (
+      <button
+        title="rittyType"
+        className={`flex justify-center items-center rounded-[24px] bg-[#f9f9f9] w-[192px] h-[192px] ${
+          selectedRitty === rittyType
+            ? "border-[3px] border-[#6B65FF] bg-[#eeedf9]"
+            : ""
+        }`}
+        onClick={() => setSelectedRitty(rittyType)}
+      >
+        <Image src={rittyImg} alt="ritty"></Image>
+      </button>
+    );
+  };
 
-export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="flex justify-center items-center h-full w-full flex-col gap-[65px]">
+      <RittyLogo isHero />
+      {/* 흰색 Content Section */}
+      <div className="color-white w-[880px] h-fit bg-[#ffffff] rounded-[44px] border-[3px] border-[#AEABF9] p-[32px] flex flex-col justify-center pt-[60px] items-center">
+        <div className="">
+          <h2 className="font-bold text-[26px]">
+            당신의 고양이는 어떤 고양이인가요?
+          </h2>
+          <p className="text-[#2c2d3883]">
+            조금 전에 찾았던 당신의 고양이를 선택해주세요.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex gap-4 mt-[45px] mb-[26px]">
+          <RittyTypeSelect rittyType={"gray"} rittyImg={GrayRitty} />
+          <RittyTypeSelect rittyType={"yellow"} rittyImg={YellowRitty} />
+          <RittyTypeSelect rittyType={"white"} rittyImg={WhiteRitty} />
+          <RittyTypeSelect rittyType={"black"} rittyImg={BlackRitty} />
+        </div>
+        <Button
+          text="이 고양이가 맞아요!"
+          onClick={() => router.push("/frame?ritty=" + selectedRitty)}
+          disabled={selectedRitty === null}
+        ></Button>
+      </div>
+    </main>
   );
-}
+};
+
+export default IndexPage;
